7f8fb6e9515fbe839ab8b83b75b622c1c585dfac
Faiz Ali <fali@bsd220.org>
2022-11-11 09:47:31 -0600

    I wrote code to decide who won as I created a win message also wrote some code to check for the winner .

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -9,41 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
-            
-            
             Text("Tic Tac Toe")
                 .fontWeight(.bold)
                 .font(.title)
-            
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing:  15), count: 3), spacing: 15) {
                 ForEach(0..<9) { index in
                     ZStack {
                         Color.blue
+                        Color.white
+                            .opacity(moves[index] == "" ? 1 : 0 )
                         Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
                     }
+                    
                     .frame(width: 120, height:  120, alignment:  .center)
                     .cornerRadius(30)
                     .onTapGesture {
-                        moves[index] = "X"
+                        withAnimation(.default) {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "O"
+                                xTurn.toggle()
+                            }
+                        }
                     }
+                    .rotation3DEffect(.init(degrees: moves[index] != "" ? 180 : 0), axis: (x: 0.0, y : 1.0, z: 0.0))
                 }
                 
+            }
+        }
+        preferredColorScheme(.dark)
+            .alert(isPresented:$gameOver) {
                 
-                
+                Alert(title: Text(winMessage))
                 
             }
-            .preferredColorScheme(.dark)
-            .padding()
+            .onChange(of: moves) { newValue in
+                checkForWinner()
+                
+            }
+    }
+    private func checkForWinner() {
+        checkLine(a:0, b: 1, c: 2) 
+        }
+    private func checkLine(a: Int, b: Int, c: Int) {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            winMessage = "\(moves[a]) is the winner!"
+            gameOver = true
+    }
+    
         }
     }
+    
+    
+    
+    
+    
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
         }
     }
-}
+    
+    
+
+
