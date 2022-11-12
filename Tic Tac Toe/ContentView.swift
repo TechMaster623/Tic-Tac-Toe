c468cd762bc57e8b4f606e008a97e02e2e1dd25e
Faiz Ali <fali@bsd220.org>
2022-11-11 20:30:44 -0600

    Cleaned up code

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
         preferredColorScheme(.dark)
             .alert(isPresented:$gameOver) {
                 
-                Alert(title: Text(winMessage))
+                Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play again")))
                 
             }
             .onChange(of: moves) { newValue in
@@ -56,6 +56,13 @@ struct ContentView: View {
     
     private func checkForWinner() {
         checkLine(a:0, b: 1, c: 2)
+        checkLine(a: 3, b: 4, c: 5)
+        checkLine(a: 6, b: 7, c: 8)
+        checkLine(a: 0, b: 3, c: 6)
+        checkLine(a: 1, b: 4, c: 7)
+        checkLine(a: 2, b: 5, c: 8)
+        checkLine(a: 0, b: 4, c: 8)
+        checkLine(a: 2, b: 4, c: 6)
     }
     
     private func checkLine(a: Int, b: Int, c: Int) {
